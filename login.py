80b802351de699b2608d9e5ba4c7553a91d5f85e
CesarPr0g3123 <cesargamecesar@gmail.com>
2025-08-12 14:41:19 -0300

    Add files via upload

---
+++ login.py
@@ -0,0 +1,11 @@
+print("---------------")
+print("--- Sistema de LOGIN ----")
+print("---------------")
+
+usuario = input("Digite seu usuario: ")
+senha = input("Digite sua senha: ")
+
+if usuario == "Cesar" and senha == "1233":
+    print("Usuario logado")
+else:
+    print("Usuario não logado")
